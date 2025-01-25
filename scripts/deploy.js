8a82f94d6297434c3cf95b1878a3200d550b90bc
Abhay Madaan <madaanabhay9@gmail.com>
2025-01-25 12:29:33 +0530

    SC part done

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -1,11 +1,29 @@
 const hre = require("hardhat")
 
 const tokens = (n) => {
-  return ethers.utils.parseUnits(n.toString(), 'ether')
+  return hre.ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
 async function main() {
+  const [deployer] = await hre.ethers.getSigners();
+  const Name = "Dappcord"
+  const Symbol = "DC"
 
+
+  const Dappcord = await hre.ethers.getContractFactory("Dappcord");
+  const dappcord = await Dappcord.deploy(Name, Symbol);
+  await dappcord.deployed();
+
+  console.log("Deployed contract address", dappcord.address);
+
+  // create 3 channels
+  const channels= ["general", "intro", "jobs"];
+  const costs = [tokens(1), tokens(2), tokens(3)]
+  for (let index = 0; index < channels.length; index++) {
+    const transaction = await dappcord.connect(deployer).createChannel(channels[index], costs[index]);
+    await transaction.wait();
+    console.log("channel ", channels[index], " is created")
+  }
 }
 
 main().catch((error) => {
